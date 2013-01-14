e03bb3fd54e7c028fd39d841f3f58a8390708780
Ronald Holshausen <rholshausen@dius.com.au>
2013-01-14 11:33:51 +1100

    got the webserver responding with html and js files from the web package

--- backend/server/web_server.dart
+++ backend/server/web_server.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+//import 'package:logging/logging.dart';
 
   
   send404(HttpResponse response) {
@@ -11,17 +12,30 @@ import 'dart:io';
   
   startServer() {
 
+    print("Starting Dart server 127.0.0.1:8080");
+
     var server = new HttpServer();
     server.listen('127.0.0.1', 8080);
     server.defaultRequestHandler = (HttpRequest request, HttpResponse response) {
       var path = request.path == '/' ? '/index.html' : request.path;
-      response.headers.set(HttpHeaders.ACCEPT,'application/json');
-      response.outputStream.write('{a: 5s}'.charCodes);
-      response.outputStream.close();
+
+      var file = new File("../web$path");
+      if (file.existsSync()) {
+        print("$path --> ${file.fullPathSync()}");
+        file.openInputStream().pipe(response.outputStream);
+      } else {
+        response.headers.set(HttpHeaders.ACCEPT,'application/json');
+        response.outputStream.write('{a: 5s}'.charCodes);
+        response.outputStream.close();
+      }
+
   };
   }
   
   main() {
+//      Logger root = Logger.root;
+//      root.level = Level.INFO;
+
       startServer();
   }
   
