de3cdb692f4489c6ca72e6a8ea2381e2559fde6e
caoilte dunne <cdunne@dius.com.au>
2013-01-14 11:41:33 +1100

    To see what happens

--- backend/server/web_server.dart
+++ backend/server/web_server.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'samplenoun_controller.dart';
+//import 'package:/logging/logging.dart';
 
   
   send404(HttpResponse response) {
@@ -8,15 +10,30 @@ import 'dart:io';
     response.outputStream.writeString("404: File not found");
     response.outputStream.close();
   }
+ 
+
+
   
   startServer() {
+//    Logger root = Logger.root;
+//    root.level = Level.INFO;
+//    root.info("here is a logging message");
+
 
     var server = new HttpServer();
     server.listen('127.0.0.1', 8080);
     server.defaultRequestHandler = (HttpRequest request, HttpResponse response) {
       var path = request.path == '/' ? '/index.html' : request.path;
+      StringBuffer sb = new StringBuffer('Hello, caoilte your path is ');
+      sb.add(path);
+      print(sb);
+
       response.headers.set(HttpHeaders.ACCEPT,'application/json');
-      response.outputStream.write('{a: 5s}'.charCodes);
+      if(SamplenounController.isSampleNoun.hasMatch(path)){
+        new SamplenounController().handleSampleNoun(request,response);
+      } else{
+        response.outputStream.write('{a: 5s}'.charCodes);
+      }
       response.outputStream.close();
   };
   }
