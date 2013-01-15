a60b581da1fb6bcd792b58a2658db8391637890c
caoilte dunne <cdunne@dius.com.au>
2013-01-15 11:22:21 +1100

    All Crud actions taking place

--- backend/lib/todo_controller.dart
+++ backend/lib/todo_controller.dart
@@ -11,6 +11,8 @@ class TodoController {
   static final isTodoRequest = new RegExp(r'.*todos(/.*)?');
 
   static final isBaseTodoRequest = new RegExp(r'.*todos$');
+
+  static final isASpecficTodoRequest = new RegExp(r'.*todos/(\d*)$');
   
   static Map todoList = new Map();
 
@@ -22,23 +24,34 @@ class TodoController {
     //var sampleNounId = isTodoRequest.firstMatch(request.path)[1];
     print("handling a todo request");
     print("matches isBaseTodoRequest ${isBaseTodoRequest.hasMatch(request.path)}");
+    print("matches isASpecficTodoRequest ${isASpecficTodoRequest.hasMatch(request.path)}");
     
     if(isBaseTodoRequest.hasMatch(request.path)){
-      if(request.method == 'GET'){
-
-        print("the list beiung passed back ${todoList}");
-        response.contentLength = JSON.stringify(todoList).length;
-        response.outputStream.write(JSON.stringify(todoList).charCodes);
-      }else if (request.method == 'POST'){
-
-        print("adding ${request.queryParameters["payload"]} to my list");
-        todoList[(nextTodoKey++).toString()] = request.queryParameters["payload"];
+        if(request.method == 'GET'){
+          print("the list beiung passed back ${todoList}");
+          response.contentLength = JSON.stringify(todoList).length;
+          response.outputStream.write(JSON.stringify(todoList).charCodes);
+        }else if (request.method == 'POST'){
+  
+          print("adding ${request.queryParameters["payload"]} to my list");
+          todoList[(nextTodoKey++).toString()] = request.queryParameters["payload"];
+        }
+      } else if(isASpecficTodoRequest.hasMatch(request.path)){
+        String matchingTodo = isASpecficTodoRequest.firstMatch(request.path)[1];
+        print("is a specfic request for ${matchingTodo}");
+        if(request.method == 'GET'){
+          String disiredTodo = todoList[matchingTodo];
+          print("the requested todo ${disiredTodo} ");
+          response.contentLength = JSON.stringify(disiredTodo).length;
+          response.outputStream.write(JSON.stringify(disiredTodo).charCodes);
+        }
+        else if(request.method == 'DELETE'){
+          String disiredTodo = todoList.remove(matchingTodo);
+        }        
+        else if(request.method == 'PUT'){
+          String disiredTodo = todoList[matchingTodo] = request.queryParameters["payload"];
+        }          
       }
-      else{
-        print("it is smothing else");
-      }
-    }
-    
   }
   
 
--- backend/test/todo_controller/todo_controller.dart
+++ backend/test/todo_controller/todo_controller.dart
@@ -9,14 +9,24 @@ import 'package:backend/todo_controller.dart';
 
 
 main() {
-  test('matches correct get url with id ', () =>
-      expect(TodoController.isTodoRequest.hasMatch('/todo'),
+  test('matches correct base url ', () =>
+      expect(TodoController.isTodoRequest.hasMatch('/todos'),
+             isTrue)
+  );  
+  
+  test('matches correct base url wit id', () =>
+      expect(TodoController.isTodoRequest.hasMatch('/todos/12'),
              isTrue)
   );  
   
   test('does not match incorrect url ', () =>
       expect(TodoController.isTodoRequest.hasMatch('/notcorrectstuff/3'),
           isFalse)
+  );
+  
+  test('gets id from spefic todo url ', () =>
+      expect(TodoController.isASpecficTodoRequest.firstMatch('/todos/3')[1],
+          equals('3'))
   );
   
   
